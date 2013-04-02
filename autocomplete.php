68b366b3f80e5b0e92c347c28af2c035dadc3e4c
rgriffith <rgriffith@0b8f769d-8bed-442f-93e2-8613d1913879>
2013-04-02 17:00:34 +0000

    add some parameter validation and authority checking

--- autocomplete.php
+++ autocomplete.php
@@ -1,37 +1,44 @@
 <?php
-
 include '../../mainfile.php';
+$xoopsLogger->activated = false; // this corrupts the reading of the json return if left activated with xoops debug on
+
 //$GLOBALS['xoopsOption']['template_main'] = 'gwreports_reportview.html';
 include(XOOPS_ROOT_PATH."/header.php");
-$table = XOOPS_DB_PREFIX."_gwreports_parameter";
-$parameter_id=$_REQUEST['parameter_id'];
+include('include/common.php');
 
+$parameter_id=intval($_REQUEST['parameter_id']);
+// really should check this to make sure the parameter belongs to a 
+// report the user is allowed to access to prevent data disclosure
 
-$sql="SELECT * FROM $table where parameter_id=$parameter_id";
+$sql='SELECT report, parameter_sqlchoice FROM '. $xoopsDB->prefix('gwreports_parameter') ." where parameter_id={$parameter_id}";
 $q=$xoopsDB->query($sql);
 $sqlchoice='';
 while($r=$xoopsDB->fetchArray($q)){
-	$sqlchoice=$r['sqlchoice'];
+	$sqlchoice=$r['parameter_sqlchoice'];
+	$report_id=$r['report'];
+}
 
+if(!($report_id) || $sqlchoice=='') die;
+if(!checkUserReportAccess($report_id)) die;
+
+$parmtags='{$xpfx}';
+$parmsubs=$xoopsDB->prefix('').'_';
+$sqlchoice=str_replace($parmtags, $parmsubs, $sqlchoice);
+
+	$str=cleaner($_REQUEST['term']);
+	if(strpos($str,'%')===false) $str='%'.$str.'%';
+	$str=dbescape($str);
+
+	$sqlchoice="SELECT * FROM ({$sqlchoice}) a where a.label LIKE  '{$str}' order by a.label limit 0,30";
+
+	$query=$xoopsDB->query($sqlchoice);
+	$data=array();
+
+	while($row=$xoopsDB->fetchArray($query)){
+		$data[]=array('value'=> $row['value'],"label"=>$row['label']);
 	}
-
-if($sqlchoice=='')
-die;
-else{
-$str=$_REQUEST['term'];
-
- $sqlchoice="SELECT * FROM ($sqlchoice) a where a.label LIKE  '%$str%' order by a.label limit 0,30";
-
-		$query=$xoopsDB->query($sqlchoice);
-				$data=array();
-
-		while($row=$xoopsDB->fetchArray($query)){
 		
-				$data[]=array('value'=> $row['value'],"label"=>$row['label']);
-			}
-		
-			echo json_encode($data);
-	
-	
-	}
+	echo json_encode($data);
+	exit;	
+?>
 
