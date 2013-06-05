473b8d2ecc3a3b56d38e915c41f5d69122d30c70
rgriffith <rgriffith@0b8f769d-8bed-442f-93e2-8613d1913879>
2013-06-05 19:20:56 +0000

    Use standard XOOPS icons if available

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,40 @@
 <?php
 // @version    $Id$
+if(file_exists(XOOPS_ROOT_PATH.'/Frameworks/moduleclasses/icons/32/about.png')) {
+$pathIcon32='../../Frameworks/moduleclasses/icons/32';
+
+$adminmenu[1] = array(
+	'title'	=> _MI_GWREPORTS_ADMENU ,
+	'link'	=> 'admin/index.php' ,
+	'icon'	=> $pathIcon32.'/home.png'
+) ;
+
+$adminmenu[] = array(
+	'title'	=> _MI_GWREPORTS_AD_ABOUT ,
+	'link'	=> 'admin/about.php' ,
+	'icon'	=> $pathIcon32.'/about.png'
+) ;
+
+$adminmenu[] = array(
+	'title'	=> _MI_GWREPORTS_AD_TOPIC ,
+	'link'	=> 'admin/topics.php' ,
+	'icon'	=> $pathIcon32.'/category.png'
+) ;
+
+$adminmenu[] = array(
+	'title'	=> _MI_GWREPORTS_AD_REPORT ,
+	'link'	=> 'admin/reports.php' ,
+	'icon'	=> $pathIcon32.'/index.png'
+) ;
+
+$adminmenu[] = array(
+	'title'	=> _MI_GWREPORTS_AD_EXPLORE ,
+	'link'	=> 'admin/explore.php' ,
+	'icon'	=> $pathIcon32.'/list.png'
+) ;
+
+} else {
+
 $adminmenu[1] = array(
 	'title'	=> _MI_GWREPORTS_ADMENU ,
 	'link'	=> 'admin/index.php' ,
@@ -30,4 +65,5 @@ $adminmenu[] = array(
 	'icon'	=> 'images/admin/explore.png'
 ) ;
 
+}
 ?>
